7f2f3d373f42c6f16f5bcbd09e46cb4a4ae2ab10
Geert <geertvk89@hotmail.com>
2020-10-25 15:25:14 +0100

    Updates

---
+++ precache-manifest.409389bebf11d154c9cc8def96df9747.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f86639a37091dcb096af71e863b72a",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "37988ec85fd9674b94ea",
+    "url": "/robo/static/css/main.1cfe6e26.chunk.css"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/robo/static/js/2.907bdc06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.907bdc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37988ec85fd9674b94ea",
+    "url": "/robo/static/js/main.d5503342.chunk.js"
+  },
+  {
+    "revision": "45b0e2f51704fab4b249",
+    "url": "/robo/static/js/runtime-main.37125452.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
